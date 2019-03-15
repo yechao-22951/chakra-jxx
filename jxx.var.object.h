a2ba8ea902c1350f44b206c0b170e39061fde863
yechao <20902129@qq.com>
2019-03-16 00:46:59 +0800

    js

--- jxx.var.object.h
+++ jxx.var.object.h
@@ -1,230 +1,229 @@
 #pragma once
 
-#include "jxx.var.value.h"
 #include "jxx.var.primitive.h"
+#include "jxx.var.value.h"
 
-namespace js {
+namespace jxx {
 
-	struct content_t {
-		ChakraBytePtr	data = 0;
-		uint32_t		size = 0;
+struct contents_t {
+    ChakraBytePtr data = 0;
+    uint32_t size = 0;
 
-		operator ChakraBytePtr* () {
-			return &data;
-		}
-		operator uint32_t* () {
-			return &size;
-		}
-	};
+    operator ChakraBytePtr *() { return &data; }
+    operator uint32_t *() { return &size; }
+};
 
-	using PropId = value_ref_t;
-	using symbol_t = value_ref_t;
+using PropId = js_value_t;
+using symbol_t = js_value_t;
 
-	PropId make_prop_id(const wchar_t* name) {
-		JsValueRef out;
-		auto err = JsGetPropertyIdFromName(name, &out);
-		if (err) return JxxException(ERR_MSG(JxxOutOfMemory));
-		return out;
-	}
-	PropId make_prop_id(symbol_t sym) {
-		JsValueRef out;
-		auto err = JsGetPropertyIdFromSymbol(sym, &out);
-		if (err) return JxxException(ERR_MSG(JxxOutOfMemory));
-		return out;
-	}
-
-	template <uint64_t AcceptableTypeMark_>
-	class object_accessor_ : public value_ref_t {
-	public:
-		enum { __required_type_mask__ = AcceptableTypeMark_ };
-	public:
-		err_t set_property(const wchar_t* name, value_ref_t value) {}
-
-		IJxxNativeObject* GetExtenalData() {
-			void* data = nullptr;
-			auto err = JsGetExternalData(get(), &data);
-			if (err) return nullptr;
-			return (IJxxNativeObject*)data;
-		}
-
-		bool SetExtenalData(IJxxNativeObject* ptr) {
-			auto old = GetExtenalData();
-			if (old) old->Release();
-			if (!ptr) return true;
-			auto err = JsSetExternalData(get(), ptr);
-			if (err) return false;
-			ptr->AddRef();
-			return true;
-		}
-
-		JsContextRef GetContext() {
-			JsContextRef ctx = JS_INVALID_REFERENCE;
-			JsGetContextOfObject(get(), &ctx);
-			return ctx;
-		}
-
-		bool WhenCollect(void* data, JsObjectBeforeCollectCallback pcb) {
-			return !JsSetObjectBeforeCollectCallback(get(), data, pcb);
-		}
-
-		class Property
-		{
-		protected:
-			value_ref_t	this_;
-			PropId	prop_id_;
-
-		public:
-			Property() {}
-			Property(value_ref_t target, PropId prop) : this_(target), prop_id_(prop) {
-			}
-			void operator = (const value_ref_t& val)
-			{
-				CXX_EXCEPTION_IF(JsSetProperty(this_, prop_id_, val, true));
-			}
-			operator value_ref_t() const
-			{
-				JsValueRef out;
-				CXX_EXCEPTION_IF(JsGetProperty(this_, prop_id_, out.address()));
-				return value_ref_t(out);
-			}
-		};
-
-		Property operator[](const __prototype__& prop_id)
-		{
-			Property X(*this, make_prop_id(L"__proto__"));
-			return X;
-		}
-
-		Property operator[](PropId prop_id)
-		{
-			return Property(*this, prop_id);
-		}
-
-		template< typename T>
-		Property operator[](T&& x)
-		{
-			return Property(*this, make_prop_id(x));
-		}
-
-		bool SetProperty(PropId pid, const value_ref_t& v) {
-			return JsSetProperty(*this, pid, v, false) == JsNoError;
-		}
-		value_ref_t GetProperty(PropId pid) {
-			JsValueRef out;
-			CXX_EXCEPTION_IF(JsGetProperty(*this, pid, &out));
-			return out;
-		}
-		value_ref_t GetPrototype() {
-			JsValueRef out;
-			CXX_EXCEPTION_IF(JsGetPrototype(*this, out));
-			return out;
-		}
-		JsErrorCode SetPrototype(value_ref_t proto) {
-			return (JsSetPrototype(*this, proto));
-		}
-	};
-
-
-
-	class array_accessor_ : public object_accessor_<_Array> {
-	public:
-		err_t set_item(const wchar_t* name, value_ref_t value) {}
-		value_ref_t get_item(const wchar_t* name) {}
-		err_t length(value_ref_t value) {}
-		value_ref_t operator[](int index) {}
-	};
-
-	using Object = base_value_<object_accessor_<_Object>>;
-	using AnyObject = base_value_<object_accessor_<_AnyObject>>;
-	using Array = base_value_<array_accessor_>;
-
-	template <JsTypedArrayType ElemType_> struct ArrayNativeType;
-
-	template <> struct ArrayNativeType<JsArrayTypeFloat64> {
-		using value_type = double;
-	};
-	template <> struct ArrayNativeType<JsArrayTypeFloat32> {
-		using value_type = float;
-	};
-	template <> struct ArrayNativeType<JsArrayTypeInt8> {
-		using value_type = int8_t;
-	};
-	template <> struct ArrayNativeType<JsArrayTypeUint8> {
-		using value_type = uint8_t;
-	};
-	template <> struct ArrayNativeType<JsArrayTypeUint8Clamped> {
-		using value_type = uint8_t;
-	};
-	template <> struct ArrayNativeType<JsArrayTypeInt16> {
-		using value_type = int16_t;
-	};
-	template <> struct ArrayNativeType<JsArrayTypeUint16> {
-		using value_type = uint16_t;
-	};
-	template <> struct ArrayNativeType<JsArrayTypeInt32> {
-		using value_type = int32_t;
-	};
-	template <> struct ArrayNativeType<JsArrayTypeUint32> {
-		using value_type = uint32_t;
-	};
-
-	template <typename ElementType_> struct ElementTypeOfNativeType;
-
-	template <> struct ElementTypeOfNativeType<int8_t> {
-		enum { type_mask = 1 << JsArrayTypeInt8 };
-	};
-	template <> struct ElementTypeOfNativeType<uint8_t> {
-		enum { type_mask = (1 << JsArrayTypeUint8) | (1 << JsArrayTypeUint8Clamped) };
-	};
-	template <> struct ElementTypeOfNativeType<int16_t> {
-		enum { type_mask = 1 << JsArrayTypeInt16 };
-	};
-	template <> struct ElementTypeOfNativeType<uint16_t> {
-		enum { type_mask = 1 << JsArrayTypeUint16 };
-	};
-	template <> struct ElementTypeOfNativeType<int32_t> {
-		enum { type_mask = 1 << JsArrayTypeInt32 };
-	};
-	template <> struct ElementTypeOfNativeType<uint32_t> {
-		enum { type_mask = 1 << JsArrayTypeUint32 };
-	};
-	template <> struct ElementTypeOfNativeType<float> {
-		enum { type_mask = 1 << JsArrayTypeFloat32 };
-	};
-	template <> struct ElementTypeOfNativeType<double> {
-		enum { type_mask = 1 << JsArrayTypeFloat64 };
-	};
-
-	// template < typename NativeElememtType_>
-	// class _typed_array : public value_ref_t {
-	// public:
-	//	enum { __required_type_mask__ =
-	//ElementTypeOfNativeType<NativeElememtType_>::type_mask }; 	using value_type =
-	//ArrayNativeType<ElememtType_>; public: 	operator [] () {
-
-	//	}
-	//};
-
-	// using TypedArray = base_value_< _array >;
-
-}; // namespace js
-
-
-JXXAPI JsValueRef JxxCreateObject(JsValueRef prototype) {
-	JsValueRef out = nullptr;
-	auto err = JsCreateObject(&out);
-	if (err) return nullptr;
-	if (prototype) {
-		err = JsSetPrototype(out, prototype);
-		if (err) return nullptr;
-	}
-	return out;
+PropId make_prop_id(const wchar_t *name) {
+    JsValueRef out;
+    auto err = JsGetPropertyIdFromName(name, &out);
+    if (err)
+        jxx_throw_error(ERR_MSG(JxxOutOfMemory));
+    return out;
+}
+PropId make_prop_id(symbol_t sym) {
+    JsValueRef out;
+    auto err = JsGetPropertyIdFromSymbol(sym, &out);
+    if (err)
+        jxx_throw_error(ERR_MSG(JxxOutOfMemory));
+    return out;
 }
 
-JXXAPI JsValueRef JxxCreateExObject(JsValueRef prototype, IJxxNativeObject * native) {
-	JsValueRef out = nullptr;
-	auto err = JsCreateExternalObjectWithPrototype(native, JxxNativeObjectRelease, prototype, &out);
-	if (err) return nullptr;
-	if (native) native->AddRef();
-	return out;
+template <uint64_t AcceptableTypeMark_>
+class object_accessor_ : public js_value_t {
+  public:
+    enum { __required_type_mask__ = AcceptableTypeMark_ };
+
+  public:
+    err_t set_property(const wchar_t *name, js_value_t value) {}
+
+    IJxxNativeObject *GetExtenalData() {
+        void *data = nullptr;
+        auto err = JsGetExternalData(get(), &data);
+        if (err)
+            return nullptr;
+        return (IJxxNativeObject *)data;
+    }
+
+    bool SetExtenalData(IJxxNativeObject *ptr) {
+        auto old = GetExtenalData();
+        if (old)
+            old->Release();
+        if (!ptr)
+            return true;
+        auto err = JsSetExternalData(get(), ptr);
+        if (err)
+            return false;
+        ptr->AddRef();
+        return true;
+    }
+
+    JsContextRef GetContext() {
+        JsContextRef ctx = JS_INVALID_REFERENCE;
+        CXX_EXCEPTION_IF(JsGetContextOfObject(get(), &ctx));
+        return ctx;
+    }
+
+    bool WhenCollect(void *data, JsObjectBeforeCollectCallback pcb) {
+        return !JsSetObjectBeforeCollectCallback(get(), data, pcb);
+    }
+
+    class Property {
+      protected:
+        js_value_t this_;
+        PropId prop_id_;
+
+      public:
+        Property() {}
+        Property(js_value_t target, PropId prop)
+            : this_(target), prop_id_(prop) {}
+        void operator=(const js_value_t &val) {
+            CXX_EXCEPTION_IF(JsSetProperty(this_, prop_id_, val, true));
+        }
+        operator js_value_t() const {
+            JsValueRef out;
+            CXX_EXCEPTION_IF(JsGetProperty(this_, prop_id_, out.address()));
+            return js_value_t(out);
+        }
+    };
+
+    Property operator[](const __js_proto__ &prop_id) {
+        Property X(*this, make_prop_id(L"__proto__"));
+        return X;
+    }
+
+    Property operator[](PropId prop_id) { return Property(*this, prop_id); }
+
+    template <typename T> Property operator[](T &&x) {
+        return Property(*this, make_prop_id(x));
+    }
+
+    bool SetProperty(PropId pid, const js_value_t &v) {
+        return JsSetProperty(*this, pid, v, false) == JsNoError;
+    }
+    js_value_t GetProperty(PropId pid) {
+        JsValueRef out;
+        CXX_EXCEPTION_IF(JsGetProperty(*this, pid, &out));
+        return out;
+    }
+    js_value_t GetPrototype() {
+        JsValueRef out;
+        CXX_EXCEPTION_IF(JsGetPrototype(*this, out));
+        return out;
+    }
+    void SetPrototype(js_value_t proto) {
+        CXX_EXCEPTION_IF(JsSetPrototype(*this, proto));
+    }
+};
+
+class array_accessor_ : public object_accessor_<_Array> {
+  public:
+    err_t set_item(const wchar_t *name, js_value_t value) {}
+    js_value_t get_item(const wchar_t *name) {}
+    err_t length(js_value_t value) {}
+    js_value_t operator[](int index) {}
+};
+
+using Object = value_as_<object_accessor_<_Object>>;
+using AnyObject = value_as_<object_accessor_<_AnyObject>>;
+using Array = value_as_<array_accessor_>;
+
+template <JsTypedArrayType ElemType_> struct ArrayNativeType;
+
+template <> struct ArrayNativeType<JsArrayTypeFloat64> {
+    using value_type = double;
+};
+template <> struct ArrayNativeType<JsArrayTypeFloat32> {
+    using value_type = float;
+};
+template <> struct ArrayNativeType<JsArrayTypeInt8> {
+    using value_type = int8_t;
+};
+template <> struct ArrayNativeType<JsArrayTypeUint8> {
+    using value_type = uint8_t;
+};
+template <> struct ArrayNativeType<JsArrayTypeUint8Clamped> {
+    using value_type = uint8_t;
+};
+template <> struct ArrayNativeType<JsArrayTypeInt16> {
+    using value_type = int16_t;
+};
+template <> struct ArrayNativeType<JsArrayTypeUint16> {
+    using value_type = uint16_t;
+};
+template <> struct ArrayNativeType<JsArrayTypeInt32> {
+    using value_type = int32_t;
+};
+template <> struct ArrayNativeType<JsArrayTypeUint32> {
+    using value_type = uint32_t;
+};
+
+template <typename ElementType_> struct ElementTypeOfNativeType;
+
+template <> struct ElementTypeOfNativeType<int8_t> {
+    enum { type_mask = 1 << JsArrayTypeInt8 };
+};
+template <> struct ElementTypeOfNativeType<uint8_t> {
+    enum {
+        type_mask = (1 << JsArrayTypeUint8) | (1 << JsArrayTypeUint8Clamped)
+    };
+};
+template <> struct ElementTypeOfNativeType<int16_t> {
+    enum { type_mask = 1 << JsArrayTypeInt16 };
+};
+template <> struct ElementTypeOfNativeType<uint16_t> {
+    enum { type_mask = 1 << JsArrayTypeUint16 };
+};
+template <> struct ElementTypeOfNativeType<int32_t> {
+    enum { type_mask = 1 << JsArrayTypeInt32 };
+};
+template <> struct ElementTypeOfNativeType<uint32_t> {
+    enum { type_mask = 1 << JsArrayTypeUint32 };
+};
+template <> struct ElementTypeOfNativeType<float> {
+    enum { type_mask = 1 << JsArrayTypeFloat32 };
+};
+template <> struct ElementTypeOfNativeType<double> {
+    enum { type_mask = 1 << JsArrayTypeFloat64 };
+};
+
+// template < typename NativeElememtType_>
+// class _typed_array : public js_value_t {
+// public:
+//	enum { __required_type_mask__ =
+// ElementTypeOfNativeType<NativeElememtType_>::type_mask }; 	using value_type
+// = ArrayNativeType<ElememtType_>; public: 	operator [] () {
+
+//	}
+//};
+
+// using TypedArray = value_as_< _array >;
+
+}; // namespace jxx
+
+JXXAPI JsValueRef JxxCreateObject(JsValueRef prototype) {
+    JsValueRef out = nullptr;
+    auto err = JsCreateObject(&out);
+    if (err)
+        return nullptr;
+    if (prototype) {
+        err = JsSetPrototype(out, prototype);
+        if (err)
+            return nullptr;
+    }
+    return out;
+}
+
+JXXAPI JsValueRef JxxCreateExObject(JsValueRef prototype,
+                                    IJxxNativeObject *native) {
+    JsValueRef out = nullptr;
+    auto err = JsCreateExternalObjectWithPrototype(
+        native, JxxNativeObjectRelease, prototype, &out);
+    if (err)
+        return nullptr;
+    if (native)
+        native->AddRef();
+    return out;
 }
--- jxx.var.value.h
+++ jxx.var.value.h
@@ -1,204 +1,188 @@
 #pragma once
-#include "js.base.h"
+#include "jxx.api.h"
+#include <ChakraCore.h>
+#include <stdint.h>
 
-namespace js {
+namespace jxx {
 
-	using err_t = JsErrorCode;
+using err_t = JsErrorCode;
 
-	enum { JsOptional = 62, JsInvalidValue = 63 };
+enum { JsOptional = 62, JsInvalidValue = 63 };
 
-	// JsValueType
-	const uint64_t __BIT = 1;
-	const uint64_t _Null = __BIT << JsNull;
-	const uint64_t _Undefined = __BIT << JsUndefined;
-	const uint64_t _Boolean = __BIT << JsBoolean;
-	const uint64_t _Number = __BIT << JsNumber;
-	const uint64_t _String = __BIT << JsString;
-	const uint64_t _Object = __BIT << JsObject;
-	const uint64_t _Function = __BIT << JsFunction;
-	const uint64_t _Array = __BIT << JsArray;
-	const uint64_t _Buffer = __BIT << JsArrayBuffer;
-	const uint64_t _DataView = __BIT << JsDataView;
-	const uint64_t _Symbol = __BIT << JsSymbol;
-	const uint64_t _TypedArray = __BIT << JsTypedArray;
-	const uint64_t _Optional = __BIT << JsOptional;
-	const uint64_t _AnyObject =
-		_Object | _Function | _Array | _Buffer | _DataView | _TypedArray;
-	const uint64_t _Nothing = _Null | _Undefined | _Optional;
-	const uint64_t _Primitive = _Null | _Undefined | _String | _Number;
-	const uint64_t _AnyOrNothing = ~JsInvalidValue;
+// JsValueType
+const uint64_t __BIT = 1;
+const uint64_t _Null = __BIT << JsNull;
+const uint64_t _Boolean = __BIT << JsBoolean;
+const uint64_t _Undefined = __BIT << JsUndefined;
+const uint64_t _String = __BIT << JsString;
+const uint64_t _Number = __BIT << JsNumber;
+const uint64_t _Object = __BIT << JsObject;
+const uint64_t _Function = __BIT << JsFunction;
+const uint64_t _Array = __BIT << JsArray;
+const uint64_t _Buffer = __BIT << JsArrayBuffer;
+const uint64_t _DataView = __BIT << JsDataView;
+const uint64_t _Symbol = __BIT << JsSymbol;
+const uint64_t _TypedArray = __BIT << JsTypedArray;
+const uint64_t _Optional = __BIT << JsOptional;
+const uint64_t _AnyObject =
+    _Object | _Function | _Array | _Buffer | _DataView | _TypedArray;
+const uint64_t _Nothing = _Null | _Undefined | _Optional;
+const uint64_t _Primitive = _Null | _Undefined | _String | _Number;
+const uint64_t _AnyOrNothing = ~JsInvalidValue;
 
-	struct __prototype__ {};
-	static const __prototype__ __prototype__;
+struct __js_proto__ {};
+static const __js_proto__ __proto__;
 
-	class value_ref_t {
-	public:
-		JsValueRef nake_ = nullptr;
-	public:
-		value_ref_t() {};
-		value_ref_t(JsValueRef r) : nake_(r) {}
-		JsValueRef get() { return nake_; }
-		void set(JsValueRef v) { nake_ = v; }
+class js_value_t {
+  public:
+    JsValueRef nake_ = nullptr;
 
-		size_t type_id() {
-			if (!nake_)
-				return JsOptional;
-			JsValueType temp_;
-			auto err = JsGetValueType(nake_, &temp_);
-			if (err)
-				return JsInvalidValue;
-			return temp_;
-		}
+  public:
+    js_value_t(){};
+    js_value_t(JsValueRef r) : nake_(r) {}
+    JsValueRef get() { return nake_; }
+    void set(JsValueRef v) { nake_ = v; }
 
-		uint64_t type_mask() {
-			if (!nake_)
-				return __BIT << JsOptional;
-			JsValueType temp_;
-			auto err = JsGetValueType(nake_, &temp_);
-			if (err)
-				return __BIT << JsInvalidValue;
-			return __BIT << temp_;
-		}
+    size_t type_id() {
+        if (!nake_)
+            return JsOptional;
+        JsValueType temp_;
+        auto err = JsGetValueType(nake_, &temp_);
+        if (err)
+            return JsInvalidValue;
+        return temp_;
+    }
 
-		template <uint64_t TypeId_> bool is() { return type_id() == TypeId_; }
+    uint64_t type_mask() {
+        if (!nake_)
+            return __BIT << JsOptional;
+        JsValueType temp_;
+        auto err = JsGetValueType(nake_, &temp_);
+        if (err)
+            return __BIT << JsInvalidValue;
+        return __BIT << temp_;
+    }
 
-		bool is(uint64_t id) { return type_id() == id; }
+    template <uint64_t TypeId_> bool is() { return type_id() == TypeId_; }
 
-		template <uint64_t TypeMask_> bool is_one_of() {
-			return (type_mask() & TypeMask_) != 0;
-		}
+    bool is(uint64_t id) { return type_id() == id; }
 
-		bool is_one_of(uint64_t mask) { return (type_mask() & mask) != 0; }
+    template <uint64_t TypeMask_> bool is_one_of() {
+        return (type_mask() & TypeMask_) != 0;
+    }
 
-		operator JsValueRef() const { return nake_; }
-		operator bool() const { return nake_ != nullptr; }
-	};
+    bool is_one_of(uint64_t mask) { return (type_mask() & mask) != 0; }
 
-	struct call_info_t {
-		JsValueRef callee;
-		bool is_new;
-		JsValueRef self;
-		JsValueRef* args;
-		size_t argc;
-		void* cookie;
-		JsValueRef returnValue;
-	};
+    operator JsValueRef() const { return nake_; }
+    operator bool() const { return nake_ != nullptr; }
+};
 
-	class param_t {
-	protected:
-		JsValueRef* args = nullptr;
-		size_t argc = 0;
-	public:
-		param_t() = default;
-		param_t(const call_info_t& info, size_t i) {
-			args = i < info.argc ? info.args + i : nullptr;
-			argc = i < info.argc ? info.argc - i : 0;
-		}
-		size_t size() const {
-			return argc;
-		}
-		JsValueRef operator[](size_t index) const {
-			if (index >= argc) return nullptr;
-			return args[index];
-		}
-	};
+struct call_info_t {
+    JsValueRef callee;
+    bool is_new;
+    JsValueRef self;
+    JsValueRef *args;
+    size_t argc;
+    void *cookie;
+    JsValueRef returnValue;
+};
 
-	template <uint64_t TypeMask_> class _as_the : public value_ref_t {
-	public:
-		_as_the(JsValueRef js_val) : value_ref_t(js_val) {
-			if (!is_one_of<TypeMask_>())
-				return JxxException(ERR_MSG(JxxErrorTypeMismatch));
-		}
+class the_param_t {
+  protected:
+    JsValueRef *args = nullptr;
+    size_t argc = 0;
 
-		_as_the(const value_ref_t& right) : value_ref_t(right) {
-			if (!is_one_of<TypeMask_>())
-				return JxxException(ERR_MSG(JxxErrorTypeMismatch));
-		}
+  public:
+    the_param_t() = default;
+    the_param_t(const call_info_t &info, size_t i) {
+        args = info.args + i;
+        argc = info.argc - i;
+    }
+    size_t size() const { return argc; }
+    JsValueRef operator[](size_t index) const {
+        if (index >= argc)
+            return nullptr;
+        return args[index];
+    }
+};
 
-		_as_the(const _as_the& right) : value_ref_t(right) {}
+template <uint64_t TypeMask_> class _the : public js_value_t {
+  public:
+    _the(JsValueRef js_val) : js_value_t(js_val) {
+        if (!is_one_of<TypeMask_>())
+            jxx_throw_error(ERR_MSG(JxxErrorTypeMismatch));
+    }
 
-		template <uint64_t TypeMask2_> _as_the(const _as_the& right) : _as_the(right.js_val_) {
-			if (!is_one_of<TypeMask_>())
-				return JxxException(ERR_MSG(JxxErrorTypeMismatch));
-		}
+    _the(const js_value_t &right) : js_value_t(right) {
+        if (!is_one_of<TypeMask_>())
+            jxx_throw_error(ERR_MSG(JxxErrorTypeMismatch));
+    }
 
-		_as_the(const param_t& param) {
-			JsValueRef right = param[0];
-			set(right);
-			if (!is_one_of<TypeMask_>())
-				return JxxException(ERR_MSG(JxxErrorTypeMismatch));
-		}
+    _the(const _the &right) : js_value_t(right) {}
 
-		operator value_ref_t() const { return *(value_ref_t*)this; }
-	};
+    template <uint64_t TypeMask2_>
+    _the(const _the &right) : _the(right.js_val_) {
+        if (!is_one_of<TypeMask_>())
+            jxx_throw_error(ERR_MSG(JxxErrorTypeMismatch));
+    }
 
+    _the(const the_param_t &param) {
+        JsValueRef right = param[0];
+        set(right);
+        if (!is_one_of<TypeMask_>())
+            jxx_throw_error(ERR_MSG(JxxErrorTypeMismatch));
+    }
 
-	// Accessor_ should inherit from value_ref_t
-	template <typename Accessor_, int Optional_ = 1>
-	class base_value_ : public Accessor_ {
-	public:
-		base_value_() = default;
-		base_value_(JsValueRef value) {
-			if (!set(value))
-				return JxxException(ERR_MSG(JxxErrorTypeMismatch));
-		}
-		base_value_(value_ref_t value) {
-			if (!set(value))
-				return JxxException(ERR_MSG(JxxErrorTypeMismatch));
-		}
-		base_value_(const base_value_& value) {
-			if (!set(value))
-				return JxxException(ERR_MSG(JxxErrorTypeMismatch));
-		}
-		template <class K, int O> base_value_(base_value_<K, O> value) {
-			if (!set(value))
-				return JxxException(ERR_MSG(JxxErrorTypeMismatch));
-		}
-		template <uint64_t TypeMask_> base_value_(_as_the<TypeMask_> argv) {
-			if (!set(argv))
-				return JxxException(ERR_MSG(JxxErrorTypeMismatch));
-		}
+    operator js_value_t() const { return *(js_value_t *)this; }
+};
 
-		bool set(value_ref_t value) {
-			if (!value.is_one_of(Accessor_::__required_type_mask__ |
-				(Optional_ ? _Optional : 0)))
-				return false;
-			Accessor_::set(value.get());
-			return true;
-		}
-		bool set(base_value_ value) {
-			set(value.get());
-			return true;
-		}
-		template <typename K, int O> bool set(base_value_<K, O> value) {
-			return set((value_ref_t)value);
-		}
+// Accessor_ should inherit from js_value_t
+template <typename Accessor_, int Optional_ = 1>
+class value_as_ : public Accessor_ {
+  public:
+    value_as_() = default;
+    value_as_(js_value_t value) {
+        if (!set(value))
+            jxx_throw_error(ERR_MSG(JxxErrorTypeMismatch));
+    }
+    value_as_(const value_as_ &value) {
+        if (!set(value))
+            jxx_throw_error(ERR_MSG(JxxErrorTypeMismatch));
+    }
+    template <class K, int O> value_as_(value_as_<K, O> value) {
+        if (!set(value))
+            jxx_throw_error(ERR_MSG(JxxErrorTypeMismatch));
+    }
+    template <uint64_t TypeMask_> value_as_(_the<TypeMask_> argv) {
+        if (!set(argv))
+            jxx_throw_error(ERR_MSG(JxxErrorTypeMismatch));
+    }
 
-		template <uint64_t TypeMask_> bool set(_as_the<TypeMask_> argv) {
-			return set((value_ref_t)argv);
-		}
-		base_value_* operator -> () {
-			return this;
-		}
-		uint32_t AddRef() {
-			if (!get()) return 0;
-			uint32_t rc = 0;
-			JsAddRef(get(), &rc);
-			return rc;
-		}
-		uint32_t Release(Context ctx) {
-			if (!get()) return 0;
-			uint32_t rc = 0;
-			JsAddRef(get(), &rc);
-			return rc;
-		}
-	};
+    bool set(js_value_t value) {
+        if (!value.is_one_of(Accessor_::__required_type_mask__ |
+                             (Optional_ ? _Optional : 0)))
+            return false;
+        Accessor_::set(value.get());
+        return true;
+    }
+    bool set(value_as_ value) {
+        set(value.get());
+        return true;
+    }
+    template <typename K, int O> bool set(value_as_<K, O> value) {
+        return set((js_value_t)value);
+    }
 
-	class value_accessor_ : public value_ref_t {
-	public:
-		enum { __required_type_mask__ = _AnyOrNothing };
-	};
+    template <uint64_t TypeMask_> bool set(_the<TypeMask_> argv) {
+        return set((js_value_t)argv);
+    }
+};
 
-	using Value = base_value_<value_accessor_>;
+class value_accessor_ : public js_value_t {
+  public:
+    enum { __required_type_mask__ = _AnyOrNothing };
+};
 
-}; // namespace js
+using Value = value_as_<value_accessor_>;
+
+}; // namespace jxx
